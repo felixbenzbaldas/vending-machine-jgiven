c327c300093a61704672e876ac8b983cb8f72450
Felix <f.benz-baldas@hotmail.de>
2024-02-26 16:59:59 +0100

    update html report

--- docs/data/metaData.js
+++ docs/data/metaData.js
@@ -1,1 +1,1 @@
-jgivenReport.setMetaData({"created":"Feb 26, 2024, 4:42:15 PM","version":"1.3.1-240a5794","title":"JGiven Report","data":["data0.js"],"showThumbnails":true} );
+jgivenReport.setMetaData({"created":"Feb 26, 2024, 4:59:22 PM","version":"1.3.1-240a5794","title":"JGiven Report","data":["data0.js"],"showThumbnails":true} );
